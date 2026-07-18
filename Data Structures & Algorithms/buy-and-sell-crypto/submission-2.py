b1b930038849e736a77fd3852923e74330bdcb14
naligant <117927298+naligant@users.noreply.github.com>
2026-07-18 17:40:25 -0500

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        start = 0
+        end = 1
+        max_ = 0
+        while end != len(prices):
+            if prices[start] >= prices[end]:
+                start = end
+            elif prices[start] < prices[end]:
+                if prices[end] - prices[start] > max_:
+                    max_ = prices[end] - prices[start]
+            end += 1
+        return max_
